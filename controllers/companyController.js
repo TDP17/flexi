bc63cd85f1aaffb1cebd0e36f4bb596f919a98a6
Achyut Shukla <achyut.shukla.btech2020@sitpune.edu.in>
2022-10-30 22:29:47 +0530

    add middleware errors

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -18,15 +18,19 @@ export const getAllCompanies = async (req, res, next) => {
   } catch (error) {
     // res.status(400).json({ error });
     next(createError(400, error.message));
+    return;
   }
 };
 
-export const getAllCompaniesByStatus = async (req, res) => {
+export const getAllCompaniesByStatus = async (req, res, next) => {
   logger.info("On get all companies by status route");
 
   const errors = validationResult(req);
-  if (!errors.isEmpty())
-    return res.status(400).json({ error: errors.array()[0].msg });
+  if (!errors.isEmpty()) {
+    // return res.status(400).json({ error: errors.array()[0].msg });
+    next(createError(400, errors.array()[0].msg));
+    return;
+  }
 
   const { status } = req.params;
 
@@ -39,15 +43,19 @@ export const getAllCompaniesByStatus = async (req, res) => {
   } catch (error) {
     // res.status(400).json({ error });
     next(createError(400, error));
+    return;
   }
 };
 
-export const getCompanyById = async (req, res) => {
+export const getCompanyById = async (req, res, next) => {
   logger.info("On get company by id route");
 
   const errors = validationResult(req);
-  if (!errors.isEmpty())
-    return res.status(400).json({ error: errors.array()[0].msg });
+  if (!errors.isEmpty()) {
+    // return res.status(400).json({ error: errors.array()[0].msg });
+    next(createError(400, errors.array()[0].msg));
+    return;
+  }
 
   const { id } = req.params;
 
@@ -59,17 +67,27 @@ export const getCompanyById = async (req, res) => {
     else res.status(400).json({ error: "No company with given id found" });
   } catch (error) {
     logger.error(error);
-    if (error.errors) res.status(400).json({ error: error.errors[0].message });
-    else res.status(400).json({ error });
+    if (error.errors) {
+      // res.status(400).json({ error: error.errors[0].message });
+      next(createError(400, error.errors[0].message));
+      return;
+    } else {
+      // res.status(400).json({ error });
+      next(createError(400, error));
+      return;
+    }
   }
 };
 
-export const postCompany = async (req, res) => {
+export const postCompany = async (req, res, next) => {
   logger.info("On create company route");
 
   const errors = validationResult(req);
-  if (!errors.isEmpty())
-    return res.status(400).json({ error: errors.array()[0].msg });
+  if (!errors.isEmpty()) {
+    // return res.status(400).json({ error: errors.array()[0].msg });
+    next(createError(400, errors.array()[0].msg));
+    return;
+  }
 
   const { name, email, address, password } = req.body;
   const { banner, logo } = req.files;
@@ -99,21 +117,35 @@ export const postCompany = async (req, res) => {
         delete company.dataValues.password;
         res.status(201).json({ message: "Company created", company });
       }
-    } else res.status(500).json({ error: "Error uploading files" });
+    } else {
+      // res.status(500).json({ error: "Error uploading files" });
+      next(createError(500, "Error uploading files"));
+      return;
+    }
   } catch (error) {
     logger.error(error);
-    if (error.errors) res.status(400).json({ error: error.errors[0].message });
-    else res.status(400).json({ error });
+    if (error.errors) {
+      // res.status(400).json({ error: error.errors[0].message });
+      next(createError(400, error.errors[0].message));
+      return;
+    } else {
+      // res.status(400).json({ error });
+      next(createError(400, error));
+      return;
+    }
   } finally {
     await deleteFromFs(banner, logo);
   }
 };
 
-export const patchCompany = async (req, res) => {
+export const patchCompany = async (req, res, next) => {
   logger.info("On update company route");
 
-  if (!req.is_admin && !req.company_id)
-    return res.status(401).json({ error: "Unauthorized to use this route" });
+  if (!req.is_admin && !req.company_id) {
+    // return res.status(401).json({ error: "Unauthorized to use this route" });
+    next(createError(401, "Unauthorized to use this route"));
+    return;
+  }
 
   const { name, email, address, password } = req.body;
 
@@ -125,14 +157,20 @@ export const patchCompany = async (req, res) => {
   }
   const { id } = req.params;
 
-  if (!req.is_admin && id !== req.company_id)
-    return res.status(401).json({ error: "Unauthorized to use this route" });
+  if (!req.is_admin && id !== req.company_id) {
+    // return res.status(401).json({ error: "Unauthorized to use this route" });
+    next(createError(401, "Unauthorized to use this route"));
+    return;
+  }
 
   try {
     const company = await Company.findByPk(id);
 
-    if (!company)
-      return res.status(400).json({ error: "Company with given id not found" });
+    if (!company) {
+      // return res.status(400).json({ error: "Company with given id not found" });
+      next(createError(400, "No company with given id found"));
+      return;
+    }
 
     let encrpytedPassword = "";
     if (password) {
@@ -169,8 +207,15 @@ export const patchCompany = async (req, res) => {
     }
   } catch (error) {
     logger.error(error);
-    if (error.errors) res.status(400).json({ error: error.errors[0].message });
-    else res.status(400).json({ error });
+    if (error.errors) {
+      // res.status(400).json({ error: error.errors[0].message });
+      next(createError(400, error.errors[0].message));
+      return;
+    } else {
+      // res.status(400).json({ error });
+      next(createError(400, error));
+      return;
+    }
   } finally {
     await deleteFromFs(banner, logo);
   }
@@ -180,20 +225,29 @@ export const patchCompanyStatus = async (req, res) => {
   logger.info("On update company status route");
 
   const errors = validationResult(req);
-  if (!errors.isEmpty())
-    return res.status(400).json({ error: errors.array()[0].msg });
+  if (!errors.isEmpty()) {
+    // return res.status(400).json({ error: errors.array()[0].msg });
+    next(createError(400, errors.array()[0].msg));
+    return;
+  }
 
   const { id } = req.params;
   const { status } = req.body;
 
-  if (!req.is_admin)
-    return res.status(401).json({ error: "Unauthorized to use this route" });
+  if (!req.is_admin) {
+    // return res.status(401).json({ error: "Unauthorized to use this route" });
+    next(createError(401, "Unauthorized to use this route"));
+    return;
+  }
 
   try {
     const company = await Company.findByPk(id);
 
-    if (!company)
-      return res.status(400).json({ error: "Company with given id not found" });
+    if (!company) {
+      // return res.status(400).json({ error: "Company with given id not found" });
+      next(createError(400, "No company with given id found"));
+      return;
+    }
 
     company.status = status;
 
@@ -206,7 +260,14 @@ export const patchCompanyStatus = async (req, res) => {
     }
   } catch (error) {
     logger.error(error);
-    if (error.errors) res.status(400).json({ error: error.errors[0].message });
-    else res.status(400).json({ error });
+    if (error.errors) {
+      // res.status(400).json({ error: error.errors[0].message });
+      next(createError(400, error.errors[0].message));
+      return;
+    } else {
+      // res.status(400).json({ error });
+      next(createError(400, error));
+      return;
+    }
   }
 };
